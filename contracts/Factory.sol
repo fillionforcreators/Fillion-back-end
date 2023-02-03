20e3f76b99288dff074ad037c30c47d2c6c970ce
degencodebeast <degencodebeast@protonmail.com>
2023-02-03 17:35:15 +0100

    added natspec comments

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -4,9 +4,27 @@ pragma solidity ^0.8.4;
 import "./Collection.sol";
 import "./Profile.sol";
 
+/*
+ * @dev Factory contract for creating collections
+ */
 contract Factory is Profile {
+    /*
+     * @dev Array of all collections
+     */
     Collection[] public collections;
 
+    /*
+     * @dev Mapping of creator address to their collection addresses
+     */
+    mapping(address => address[]) public collectionsByCreator;
+
+    /*
+     * @dev Create a new collection
+     * @param _collectionName The name of the collection
+     * @param _uri The base uri for the collection
+     * @param _contents The contents of the collection
+     * @return The address of the collection
+     */
     function createCollection(
         string memory _collectionName,
         string memory _uri,
@@ -14,6 +32,33 @@ contract Factory is Profile {
     ) public onlyCreators returns (address) {
         Collection c = new Collection(_collectionName, _uri, _contents);
         collections.push(c);
+        collectionsByCreator[msg.sender].push(address(c));
         return address(c);
+    }
+
+    /*
+     * @dev Get a collection by id
+     * @param _id The id of the collection
+     * @return The address of the collection
+     */
+    function getCollectionById(uint256 _id) public view returns (address) {
+        return address(collections[_id]);
+    }
+
+    /*
+     * @dev Get all collections by a creator
+     * @param _creator The address of the creator
+     * @return An array of creator's collection addresses
+     */
+    function getCollectionsByCreator(address _creator) public view returns (address[] memory) {
+        return collectionsByCreator[_creator];
+    }
+
+    /*
+     * @dev Get all collections
+     * @return An array of all collection addresses
+     */
+    function getAllCollections() public view returns (Collection[] memory) {
+        return collections;
     }
 }
